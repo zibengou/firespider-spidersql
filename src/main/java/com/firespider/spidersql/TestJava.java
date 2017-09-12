ed7036371b4702e5be4ec686d4ef876a42957e3d
xiaotong.shi <zibengou@outlook.com>
2017-09-12 19:38:20 +0800

    spidersql project

---
+++ src/main/java/com/firespider/spidersql/TestJava.java
@@ -0,0 +1,12 @@
+package com.firespider.spidersql;
+
+/**
+ * Created by xiaotong.shi on 2017/9/12.
+ */
+public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        Test t = new Test();
+        System.out.println(t.pp());
+    }
+}
